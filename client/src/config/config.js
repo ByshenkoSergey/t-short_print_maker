300deb3cf17afdb317d22b116325d2579b77f24d
Serhii Byshenko <byshenkoS1989@gmail.com>
2024-03-28 15:26:03 +0200

    Change local URL to prod url

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,9 +1,9 @@
 const config = {
 
-  backendUrl: "http://localhost:8080/api/v1/dalle"
+  backendUrl: "https://t-short-print-maker.onrender.com/api/v1/dalle"
   
   // Please replase url if you want deploy into prodaction
-  // prodaction url: https://devswag.onrender.com/api/v1/dalle
+  // prodaction url: https://t-short-print-maker.onrender.com/api/v1/dalle
   // local url: http://localhost:8080/api/v1/dalle
 };
 
